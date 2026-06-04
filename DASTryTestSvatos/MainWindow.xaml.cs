1debb00d6b1521f45b8e8c03e220e01c71d0b91a
23ib25_svatos <23ib25_svatos@U1-05>
2026-06-04 10:15:21 +0200

    Přidejte soubory projektu.

---
+++ DASTryTestSvatos/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DASTryTestSvatos
+{
+    public partial class MainWindow : Window
+    {
+
+        private static string connectionString = "Server=alaska;Database=23IB25_SVATOS;Trusted_Connection=True;TrustServerCertificate=True";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadMovies_Click(object sender, RoutedEventArgs e)
+        {
+            using SqlConnection conn = new SqlConnection(connectionString);
+
+            string sql = "SELECT * FROM Movie";
+            
+            var data = conn.Query<Movie>(sql).ToList();
+
+            MoviesDataGrid.ItemsSource = data;
+        }
+    }
+}
---
+++ DASTryTestSvatos/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DASTryTestSvatos
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public decimal? Budget { get; set; }
+        public decimal? Revenue { get; set; }
+        public int MediaTypeId { get; set; }
+    }
+}
